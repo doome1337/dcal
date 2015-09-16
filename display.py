3b46e69560974acc4a128fe75f97422489ab2391
doome1337 <dmitry.paramonov@gmail.com>
2015-09-16 12:13:22 -0400

    Made display.py modular.
    Now, it can be ran as a method,
    or as a stand-alone utility.

--- display.py
+++ display.py
@@ -1,4 +1,5 @@
 import datetime
+import sys
 
 def time_format(time):
     s = time.seconds
@@ -18,47 +19,51 @@ def task_format(task):
     else:
         return task + (" "*(29-length))
 
-now = datetime.datetime.now()
-events = []
-print
-print "Today is " + str(now)
-print "-----"
-with file("planner.csv","r") as calendar:
-    lines = calendar.readlines()[1:]
-    for line in lines:
-        data = line.split(",")
-        due_date = datetime.datetime(int(data[1]),\
-                int(data[2]),int(data[3]),int(data[4]),\
-                int(data[5]),int(data[6]))
-        until = due_date - now
-        events.append([data[0],int(data[7]),until])
-for i in range(len(events)):
-    for j in range(i,len(events)):
-        if (events[i][1] > events[j][1]) or \
-                (events[i][2] > events[j][2] and \
-                not events[i][1] < events[j][1]):
-            temp = events[i]
-            events[i] = events[j]
-            events[j] = temp
-# A list of Completion Codes:
-# 0: Incomplete
-# 1: Complete, but needs revision/submission
-# 2: Completed, submitted.
-for i in events:
-    if i[1] == 0:
-        if i[2] > datetime.timedelta(0):
-            print '"' + task_format(i[0]) + '" incomplete. ' \
-                    + time_format(i[2]) + " left."
+def display(file_name):
+    now = datetime.datetime.now()
+    events = []
+    print
+    print "Today is " + str(now)
+    print "-----"
+    with file(file_name,"r") as calendar:
+        lines = calendar.readlines()[1:]
+        for line in lines:
+            data = line.split(",")
+            due_date = datetime.datetime(int(data[1]),\
+                    int(data[2]),int(data[3]),int(data[4]),\
+                    int(data[5]),int(data[6]))
+            until = due_date - now
+            events.append([data[0],int(data[7]),until])
+    for i in range(len(events)):
+        for j in range(i,len(events)):
+            if (events[i][1] > events[j][1]) or \
+                    (events[i][2] > events[j][2] and \
+                    not events[i][1] < events[j][1]):
+                temp = events[i]
+                events[i] = events[j]
+                events[j] = temp
+    # A list of Completion Codes:
+    # 0: Incomplete
+    # 1: Complete, but needs revision/submission
+    # 2: Completed, submitted.
+    for i in events:
+        if i[1] == 0:
+            if i[2] > datetime.timedelta(0):
+                print '"' + task_format(i[0]) + '" incomplete. ' \
+                        + time_format(i[2]) + " left."
+            else:
+                print '"' + task_format(i[0]) + '" OVERDUE! ' \
+                        + time_format(-i[2]) + " late!"
+        elif i[1] == 1:
+            if i[2] > datetime.timedelta(0):
+                print '"' + task_format(i[0]) + '" complete, but unfinished. ' \
+                        + time_format(i[2]) + " left."
+            else:
+                print '"' + task_format(i[0]) + '" OVERDUE! ' \
+                        + time_format(-i[2]) + " late!"
         else:
-            print '"' + task_format(i[0]) + '" OVERDUE! ' \
-                    + time_format(-i[2]) + " late!"
-    elif i[1] == 1:
-        if i[2] > datetime.timedelta(0):
-            print '"' + task_format(i[0]) + '" complete, but unfinished. ' \
-                    + time_format(i[2]) + " left."
-        else:
-            print '"' + task_format(i[0]) + '" OVERDUE! ' \
-                    + time_format(-i[2]) + " late!"
-    else:
-        print '"' + task_format(i[0]) + '" complete!'
-print 
+            print '"' + task_format(i[0]) + '" complete!'
+    print 
+
+if __name__ == '__main__':
+    display(sys.argv[1])
