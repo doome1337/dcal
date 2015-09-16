ff6f2e46b6915ec3ead6d4eaf31615aa80f051cd
doome1337 <dmitry.paramonov@gmail.com>
2015-09-16 17:00:13 -0400

    Minor output fixes, and documentation.
    Fixed a bug where empty parens would be prints.
    Also added documentation for formatting methods.

--- display.py
+++ display.py
@@ -2,6 +2,18 @@ import datetime
 import sys
 
 def time_format(time):
+    """Formats a timedelta in the form DD:HH:MM:SS.
+
+    @type time: timedelta
+    @rtype str
+
+    >>> time_format(datetime.timedelta(hours=10))
+    '00:10:00:00'
+    >>> time_format(datetime.timedelta(hours=25))
+    '01:01:00:00'
+    >>> time_format(datetime.timedelta(hours=4,seconds=15,minutes=26,days=7))
+    '07:04:26:15'
+    """
     s = time.seconds
     d = time.days
     minutes, seconds = divmod(s, 60)
@@ -11,6 +23,23 @@ def time_format(time):
     return "%02d:%02d:%02d:%02d" % (days, hours, minutes, seconds)
 
 def task_format(task):
+    """Formats a task name to appear correctly.
+
+    If shorter than 30 characters,
+    it is padded with spaces.
+    If longer, it cuts it down to 27 characters,
+    and adds an ellipsis.
+
+    @type time: str
+    @rtype str
+
+    >>> task_format('Hello World!')
+    'Hello World!                  '
+    >>> task_format("This line is 30 characters, yo")
+    'This line is 30 characters, yo'
+    >>> task_format("This line is way too long to fit normally.")
+    'This line is way too long t...'
+    """
     length = len(task)
     # Together with the double dash,
     # this makes 32 characters before the status.
@@ -22,7 +51,7 @@ def task_format(task):
 def display(file_name):
     now = datetime.datetime.now()
     events = []
-    print ()
+    print ("")
     print ("Today is " + str(now))
     print ("-----")
     with open(file_name,"r") as calendar:
@@ -63,7 +92,7 @@ def display(file_name):
                         + time_format(-i[2]) + " late!")
         else:
             print (task_format(i[0]) + ' -- Complete!')
-    print ()
+    print ("")
 
 if __name__ == '__main__':
     display(sys.argv[1])
