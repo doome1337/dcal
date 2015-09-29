0e001c5c5a7811bf777deb9d16c59aea023d6456
doome1337 <dmitry.paramonov@gmail.com>
2015-09-29 13:01:57 -0400

    Documentation and clean-up of clean.py

--- clean.py
+++ clean.py
@@ -1,7 +1,29 @@
-import sys
-import os
+def clean(file_name,threshold=2):
+    """Clean-up for planner files.
 
-def clean(file_name,threshold):
+    Removes any tasks whose priority is above a certain amount.
+    By default, it will remove tasks of status 2 or higher.
+
+    @type file_name: str
+    @type threshold: int
+    @rtype: None
+
+    >>> display('file.csv')
+    Today is 2015-09-28 11:17:56.072809
+    -----
+    MAT157 Non-Standard Lecture... -- Incomplete. 02:06:42:03 left.
+    MAT157 Non-Standard Lecture... -- Incomplete. 09:04:42:03 left.
+    MAT157 Problem Set 2           -- To be revised. 00:03:42:03 left.
+    asd                            -- Complete!
+    asgsad                         -- Complete!
+    >>> clean('file.csv')
+    >>> display('file.csv')
+    Today is 2015-09-28 11:17:56.074163
+    -----
+    MAT157 Non-Standard Lecture... -- Incomplete. 02:06:42:03 left.
+    MAT157 Non-Standard Lecture... -- Incomplete. 09:04:42:03 left.
+    MAT157 Problem Set 2           -- To be revised. 00:03:42:03 left.
+    """
     with open(file_name,'r') as calendar:
         lines = calendar.readlines()[1:]
         remove = {}
@@ -14,10 +36,12 @@ def clean(file_name,threshold):
                 calendar.write(line)
 
 if __name__ == '__main__':
-    __DEBUG__ = False
+    import sys
+    import os
+    __DEBUG__ = True
     if __DEBUG__:
         import display
         display.display(sys.argv[1])
-    clean(sys.argv[1],2)
+    clean(sys.argv[1])
     if __DEBUG__:
         display.display(sys.argv[1])
