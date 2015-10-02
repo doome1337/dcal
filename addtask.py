9a6930d6835d5d12b30655f521561fd7da05ecc0
doome1337 <dmitry.paramonov@gmail.com>
2015-10-02 18:27:51 -0400

    Addtask now allows for choice of status.
    Note that the default is still 'inc' for incomplete.

--- addtask.py
+++ addtask.py
@@ -1,9 +1,10 @@
 import datetime
 def add_task(args):
-    #args.cal_file
-    #args.task_name
-    #args.time
-    time = datetime.datetime.strptime(','.join(args.time),'%Y,%m,%d,%H,%M,%S')
+    # time is currently converted to a datetime 
+    # so that alternate input methods can be made, 
+    # while output format remains the same.
+    time = datetime.datetime.strptime(','.join([str(x) for x in args.time]),
+            '%Y,%m,%d,%H,%M,%S')
     with open(args.cal_file,'a') as calendar:
         calendar.write(args.task_name+","+
-                time.strftime('%Y,%m,%d,%H,%M,%S')+",inc")
+                time.strftime('%Y,%m,%d,%H,%M,%S')+","+args.status+'\n')
--- dcal.py
+++ dcal.py
@@ -11,21 +11,31 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calendar Utility')
     subparsers = parser.add_subparsers()
     display_parser = subparsers.add_parser('display',aliases=['dis'])
-    display_parser.add_argument('cal_file')
+    display_parser.add_argument('cal_file',type=str,
+            help='calendar file to display.')
     display_parser.add_argument('format_file')
     display_parser.set_defaults(func=display.display)
     clean_parser = subparsers.add_parser('clean')
-    clean_parser.add_argument('cal_file')
-    clean_parser.add_argument('format_file')
+    clean_parser.add_argument('cal_file',type=str,
+            help='calendar file to clean.')
+    clean_parser.add_argument('format_file',type=str,
+            help='format file containing all event types.')
     clean_parser.set_defaults(func=clean.clean)
     gen_parser = subparsers.add_parser('generate',aliases=['gen'])
-    gen_parser.add_argument('source_file')
-    gen_parser.add_argument('cal_file')
+    gen_parser.add_argument('source_file',type=str,
+            help='source file from which the recurring events are loaded.')
+    gen_parser.add_argument('cal_file',type=str,
+            help='calendar file to which to write the recurring tasks.')
     gen_parser.set_defaults(func=generator.gen_events)
     addtask_parser = subparsers.add_parser('add')
-    addtask_parser.add_argument('cal_file')
-    addtask_parser.add_argument('task_name')
-    addtask_parser.add_argument('time',nargs=6)
+    addtask_parser.add_argument('cal_file',type=str,
+            help='calendar file to which to write the task.')
+    addtask_parser.add_argument('task_name',type=str,
+            help='name of event or task to complete.')
+    addtask_parser.add_argument('time',nargs=6,type=int,
+            help='time of event in the form YYYY MM DD HH MM SS.')
+    addtask_parser.add_argument('-s','--status',nargs='?',default='inc',
+            type=str,help='status of task to append.')
     addtask_parser.set_defaults(func=addtask.add_task)
     args = parser.parse_args()
     args.func(args)
