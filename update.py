feda54a78ac03a438a0b6e4cf3fb03aa418868ab
doome1337 <dmitry.paramonov@gmail.com>
2015-10-12 16:21:27 -0400

    Fixed bug involving no newline during update.
    Also, made update.py PEP8 compliant.

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-def match(mode,case,task,given):
+def match(mode, case, task, given):
     if not case:
         task = task.lower()
         given = given.lower()
@@ -7,24 +7,25 @@ def match(mode,case,task,given):
     elif mode == 'start':
         return task.startswith(given)
     elif mode == 'in':
-        return task.find(given)>-1
+        return task.find(given) > -1
+
 
 def update(args):
-    with open(args.cal_file,'r') as calendar:
+    with open(args.cal_file, 'r') as calendar:
         lines = calendar.readlines()
-    with open(args.cal_file,'w') as calendar:
+    with open(args.cal_file, 'w') as calendar:
         for line in lines:
-            if not match(args.mode,args.case,
-                    line.split(',')[0],args.task_name):
+            if not match(args.mode, args.case,
+                         line.strip().split(',')[0], args.task_name):
                 calendar.write(line)
             else:
                 line_components = line.split(',')
-                if args.time == None:
+                if args.time is None:
                     time = line_components[1:7]
                 else:
                     time = [str(x) for x in args.time]
-                if args.status == None:
+                if args.status is None:
                     status = line_components[7:]
                 else:
                     status = [args.status]
-                calendar.write(','.join(line_components[0:1]+time+status))
+                calendar.write(','.join(line_components[0:1]+time+status)+'\n')
